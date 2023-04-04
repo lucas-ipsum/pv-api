17d3715013f65905231f9da7dc0063b447948cc2
lucas-ipsum <s0572357@htw-berlin.de>
2023-04-04 18:28:45 +0200

    filter wip

--- pv_api/pv_api/settings.py
+++ pv_api/pv_api/settings.py
@@ -41,8 +41,14 @@ INSTALLED_APPS = [
     'django_extensions',
     'pv_api',
     'rest_framework',
+    'django_filters',
 
 ]
+
+# DjangoFilterBackend 
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- pv_api/pv_api/views.py
+++ pv_api/pv_api/views.py
@@ -1,13 +1,16 @@
 from rest_framework import viewsets
 from .models import State
 from .serializers import StateSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class StateViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = State.objects.all()
     serializer_class = StateSerializer
-    print(serializer_class)
-    print(queryset)
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['state']
+    print(filterset_fields)
+    print('hi')
 
     def get_queryset(self):
         states = State.objects.all()
