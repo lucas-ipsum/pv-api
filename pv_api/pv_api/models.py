c61ceef5628baf308e8ca70502e90767ab1aee3b
lucas-ipsum <s0572357@htw-berlin.de>
2023-04-04 16:56:09 +0200

    Model name

--- pv_api/pv_api/models.py
+++ pv_api/pv_api/models.py
@@ -1,5 +1,8 @@
 from django.db import models 
 
 class State(models.Model):
-    state = models.CharField(max_length=4)
+    state = models.CharField(max_length=2)
     pv_yield = models.IntegerField()
+
+    def __str__(self):
+        return self.state
