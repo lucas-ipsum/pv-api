cedc9c19d278d068d275a857bfe11d5463c93dd9
lucas-ipsum <s0572357@htw-berlin.de>
2023-04-05 09:39:06 +0200

    Vercel settings removed

--- pv_api/pv_api/settings.py
+++ pv_api/pv_api/settings.py
@@ -26,7 +26,6 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -132,7 +131,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Hosting on Vercel 
-STATICFILES_DIRS = [BASE_DIR/'static',]
-STATIC_ROOT = BASE_DIR/'staticfiles'
